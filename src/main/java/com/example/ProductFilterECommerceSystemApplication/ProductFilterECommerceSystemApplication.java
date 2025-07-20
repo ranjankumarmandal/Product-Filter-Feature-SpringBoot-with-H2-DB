3401066cd47869cdf566a54fc03c453c8dacaafd
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2025-07-20 19:11:16 +0530

    Spring Boot: E-commerce product filter api application: added missing dependency for spring boot application development

--- src/main/java/com/example/ProductFilterECommerceSystemApplication/ProductFilterECommerceSystemApplication.java
+++ src/main/java/com/example/ProductFilterECommerceSystemApplication/ProductFilterECommerceSystemApplication.java
@@ -1,11 +1,13 @@
-package org.example.ProductFilterECommerceSystem;
+package com.example.ProductFilterECommerceSystemApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductFilterECommerceSystemApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ProductFilterECommerceSystemApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductFilterECommerceSystemApplication.class, args);
+	}
+
 }
---
+++ src/test/java/com/example/ProductFilterECommerceSystemApplication/ProductFilterECommerceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductFilterECommerceSystemApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductFilterECommerceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
