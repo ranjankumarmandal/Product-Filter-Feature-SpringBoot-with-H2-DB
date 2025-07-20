8c7e034e7ef6a2093d3c97b2a973e77aa4f23476
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2025-07-20 18:48:32 +0530

    Spring Boot: E-commerce product filter api application: updated respctive files for spring boot project development manually

---
+++ src/main/java/org/example/ProductFilterECommerceSystem/ProductFilterECommerceSystemApplication.java
@@ -0,0 +1,11 @@
+package org.example.ProductFilterECommerceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductFilterECommerceSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductFilterECommerceSystemApplication.class, args);
+    }
+}
